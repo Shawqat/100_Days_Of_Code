ba9abd60641f534f116da235fc7510fd235e927e
Shawqat <mahmoud.k.shawqat@gmail.com>
2026-07-19 12:55:26 +0300

    ADD Project files

---
+++ Day_020/project/main.py
@@ -0,0 +1,33 @@
+from turtle import Screen, Turtle
+import time
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height=600)
+
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+screen.tracer(0)
+# create snake body
+snake = Snake()
+
+# which direction to move
+screen.listen()
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.right, "d")
+screen.onkey(snake.left, "a")
+
+# moving snake
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+
+
+
+screen.exitonclick()
